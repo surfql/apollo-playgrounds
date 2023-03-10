e85daf7a725da4f439bdfe81b34c6a4b262ae880
Ethan McRae <ethanmcrae0@gmail.com>
2023-03-10 01:43:06 -0700

    Adds surfql config by default

--- client/src/query.tsx
+++ client/src/query.tsx
@@ -1,5 +1,11 @@
 import { gql } from '@apollo/client';
 
+/* 🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊
+ * The surfql.config.json is referencing the GraphQL types
+ * from the server setup files to aid in frontend query
+ * development!
+ * 🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊
+ */
 export const query = gql`
   query {
     users {
